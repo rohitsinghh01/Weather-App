fec02fb99994e7df237cad0d5f01da63b8a33361
Rohit <singh.rohitraj122@gmail.com>
2023-05-23 00:16:29 +0530

    node functio added

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+require("dotenv").config();
+const http = require("http");
+const fs = require("fs");
+var requests = require("requests");
 
+const homeFile = fs.readFileSync("index.html", "utf-8");
+
+const replaceVal = (tempVal, orgVal) => {
+  let temperature = tempVal.replace("{%tempVal%}", orgVal.main.temp);
+  temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
+  temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
+  temperature = temperature.replace("{%location%}", orgVal.name);
+  temperature = temperature.replace("{%country%}", orgVal.sys.country);
+  temperature = temperature.replace("{%tempStatus%}", orgVal.weather[0].main);
+  return temperature;
+};
+
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    requests(
+      `https://api.openweathermap.org/data/2.5/weather?q=Patna&units=metric&appid=${process.env.APPID}`
+    )
+      .on("data", (chunk) => {
+        const objData = JSON.parse(chunk);
+        const arrData = [objData];
+        const realTimedata = arrData
+          .map((val) => replaceVal(homeFile, val))
+          .join("");
+        res.write(realTimedata);
+      })
+
+      .on("end", (err) => {
+        if (err) return console.log("connection closed due to errors", err);
+        res.end();
+      });
+  } else {
+    res.end("File Not Found");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Server started at port 8000...");
+});
